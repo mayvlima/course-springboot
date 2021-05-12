42e72f8fda4126799438fa19f383243db78d8a08
Mayara Lima <may.veloso.lima@gmail.com>
2021-05-12 12:22:58 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -63,6 +63,10 @@ public class OrderItem {
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
